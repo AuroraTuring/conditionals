7f4c27368214933ef2e44263d53331ac1be523e4
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 14:34:32 -0600

    Fix an error

--- code_challenges/medium.rb
+++ code_challenges/medium.rb
@@ -2,10 +2,10 @@
 
 # Using the following variables:
 
-```ruby
-good_driving_record = true
-age = 24
-```
+# ```ruby
+# good_driving_record = true
+# age = 24
+#```
 
 # Write a program that evaluates the status of the variables above, and uses logic to print one of three possible messages:
 # - If the driving record is good and user is over 25 years old, they should get a discount on the car rental
