77248d5b0bd7e91a93aa5ec2c7ffc3037271a07d
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 14:08:19 -0600

    Move file

---
+++ practice.rb
@@ -0,0 +1,23 @@
+greeting = "Hello"
+
+if greeting == "Hello"
+  puts "Let's go!"
+end 
+
+
+
+age = 16
+
+if age >= 21 
+  puts "Welcome to the website!" 
+elsif age < 2 
+  puts "How are you even on this website?"
+else 
+  puts "Sorry, age restricted content."
+end
+
+
+
+# More Practice: Logical Operators with Conditions
+# Declare a variable that stores a password. In order for a password to be valid, it needs to have 8 characters or more and include the symbol #. Use logical operators and conditions to give the user feedback based on their password. If the user enters a password that is both 8 characters or more and includes the symbol #, print the statement, "Got it. Your password is: XXXXXXXX" using interpolation to confirm their password.
+
