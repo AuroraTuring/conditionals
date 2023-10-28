d268e4a7db3e6d93e35382fbdce2637be5a0901c
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 15:22:05 -0600

    Answer more practice

--- practice.rb
+++ practice.rb
@@ -21,3 +21,10 @@ end
 # More Practice: Logical Operators with Conditions
 # Declare a variable that stores a password. In order for a password to be valid, it needs to have 8 characters or more and include the symbol #. Use logical operators and conditions to give the user feedback based on their password. If the user enters a password that is both 8 characters or more and includes the symbol #, print the statement, "Got it. Your password is: XXXXXXXX" using interpolation to confirm their password.
 
+password = "abc123"
+
+if password.length >= 8 && password.include?("#") 
+  puts "Got it. Your password is: #{password}." 
+else
+  puts "Please use a # sign and 8 or more characters."
+end
