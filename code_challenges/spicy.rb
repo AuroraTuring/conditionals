9eb5f66c4786f6c02f9cebd5ae0f30dd05730588
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 15:13:06 -0600

    Add placeholders

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -3,16 +3,15 @@
 # Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer. If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.
 
 
-name = "Aurora" 
 age = 31 
 
-if age = 
+if age = x3 
   print "Fizz"
-elsif age = 
+elsif age = x5 
   print "Buzz"
-elsif age = && age = 
+elsif age = x3 && age = x5 
   print "FizzBuzz"
-else age = 
+else
   print age
 end
 
