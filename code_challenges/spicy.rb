52681b68a6908703baacdb22cc88388fb2d7d412
Aurora Sanderson <AuroraTuring@gmail.com>
2023-10-28 14:39:41 -0600

    Partially solve challenge

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -3,3 +3,16 @@
 # Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer. If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.
 
 
+name = "Aurora" 
+age = 31 
+
+if age = 
+  print "Fizz"
+elsif age = 
+  print "Buzz"
+elsif age = && age = 
+  print "FizzBuzz"
+else age = 
+  print age
+end
+
